a86b5bca4498c1a5760fe5e87813c6a6f245d4f2
Minh Dang Pham <minh.pham.024@pslib.cz>
2024-12-15 20:47:52 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,81 @@
+let a: number = 6
+let b: number = 7
+if (a === b) {
+  console.log("hello")
+} else {
+  const c: string = "I hate "
+  const d: string = "TypeScript"
+  console.log(c + d)
+}
+
+//------------------------------------------
+
+for (let e = 0; e < 2; e++) {
+  console.log(e);
+}
+for (let f = 2; f < 3; f++) {
+  console.log(f)
+  console.log(f);
+}
+let g: string = "Hello world!"
+console.log(g)
+
+//-----------------------------------------
+
+let abNegative: number = -2
+let aaNegative: number = -1
+let aZero: number = 0
+let aa: number = 1
+let ab: number = 2
+let ac: number = 3
+let ad: number = 4
+let ae: number = 5
+let af: number = 6
+let ag: number = 7
+let ah: number = 8
+let ai: number = 9
+let aj: number = 10
+let ak: number = 11
+let al: number = 12
+let am: number = 13
+let an: number = 14
+let ao: number = 15
+
+let text: string = "hello"
+let text1: string  = "4"
+let text2: string = "5"
+
+let h: number = ao % al;
+console.log(h);
+
+let i: number = ac ** ab;
+console.log(i);
+
+let j: number = Math.sqrt(144);
+console.log(j)
+
+let k: number = Math.pow(216, 1/ac)
+console.log(k)
+
+let l: number = Math.abs(abNegative)
+console.log(l)
+
+if (ae === af) {
+  console.log("ae isn't equal to af")
+} else {
+  console.log("ae is equal to af")
+}
+
+let m: boolean = ag <= ah
+console.log(m)
+
+let n: boolean = true
+let o: boolean = false
+let p: boolean = n && o
+console.log(p)
+
+let q: boolean = n || o
+console.log(q)
+
+let r: boolean = !n
+console.log(r)
